018bcbb4dee0f7d0fc7512b94dd1b2b8d777a30a
Samin-Sadaf7 <95418091+Samin-Sadaf7@users.noreply.github.com>
2024-12-05 21:54:18 +0600

    Set up main spring boot application

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,27 @@
-package org.example;
+package com.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import javax.annotation.PostConstruct;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(App.class, args);
+    }
+
+    @PostConstruct
+    public void init()
+    {
+        Logger log = LoggerFactory.getLogger(App.class);
+        log.info("Java app started");
+    }
+
+    public String getStatus() {
+        return "OK";
     }
 }
